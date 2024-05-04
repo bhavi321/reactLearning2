5fd7ae101223c1180d99135ef49b070fca00fe83
bhavi321 <bhavialkhaniya5@gmail.com>
2024-05-04 20:28:26 +0530

    Finished

---
+++ 1-todo-version-1/src/components/ToDoItem1.jsx
@@ -0,0 +1,17 @@
+function TodoItem1(){
+    return(
+        <div class="container">
+        <div class="row kg-row" >
+        <div class="col-4">Buy Milk </div>
+        <div class="col-4">07/11/23</div>
+        <div class="col-2">
+            {" "}
+            <button type="button" class="btn btn-danger kg-button">
+            Delete
+            </button>
+        </div>
+        </div>
+    </div>
+    )
+}
+export default TodoItem1
---
+++ 1-todo-version-1/src/components/TodoItem2.jsx
@@ -0,0 +1,17 @@
+function TodoItem2(){
+    return(
+        <div class="container">
+        <div class="row kg-row">
+        <div class="col-4">Go To College </div>
+        <div class="col-4">05/11/23</div>
+        <div class="col-2">
+            {" "}
+            <button type="button" class="btn btn-danger kg-button">
+            Delete
+            </button>
+        </div>
+        </div>
+    </div>
+    )
+}
+export default TodoItem2
