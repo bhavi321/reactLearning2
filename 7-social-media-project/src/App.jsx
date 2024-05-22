73cac41c1ecf88c4f077ed89875ce9fd1825e583
bhavi321 <bhavialkhaniya5@gmail.com>
2024-05-22 20:57:21 +0530

    Started project. Did setup. Added header.

---
+++ 7-social-media-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header>
+        
+      </Header>
+     
+    </>
+  )
+}
+
+export default App
