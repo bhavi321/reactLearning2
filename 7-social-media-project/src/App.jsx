9d66ad0a3c4f94a7965d234d9e54274ac534eae2
bhavi321 <bhavialkhaniya5@gmail.com>
2024-05-24 20:53:26 +0530

    Did setup for useContext and useReducer. Created data for posts.

--- 7-social-media-project/src/App.jsx
+++ 7-social-media-project/src/App.jsx
@@ -6,11 +6,13 @@ import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 import { useState } from "react";
+import PostListProvider from "./store/post-list store";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Create Post");
 
   return (
+    <PostListProvider>
     <div className="app-container">
       <Sidebar selectedTab={selectedTab} setSelectedTab = {setSelectedTab}></Sidebar>
       <div className="content">
@@ -24,6 +26,7 @@ function App() {
         <Footer></Footer>
       </div>
     </div>
+    </PostListProvider>
   );
 }
 
--- 7-social-media-project/src/store/post-list store.jsx
+++ 7-social-media-project/src/store/post-list store.jsx
@@ -1,9 +1,43 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({});
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+  const addPost = () => {};
+  const deletePost = () => {};
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to hyderabad",
+    body: "Hi Friends, I am going to hyderabad for my vacations. Hope to enjoy a lot. We will have fun.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "We are passed!!!",
+    body: "We are passed even having so much fun and not studying. Hard to believe.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
 
-const PostListProvider = () => {
-
-}
-
-export default PostListProvider; 
+export default PostListProvider;
