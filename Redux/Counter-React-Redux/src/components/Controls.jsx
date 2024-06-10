aee9b3d37a200e329fffd1c6c3823c728afa5c9c
bhavi321 <bhavialkhaniya5@gmail.com>
2024-06-10 20:59:13 +0530

    Created redux project with react. Added increment, decrement, add and subtract functionality

---
+++ Redux/Counter-React-Redux/src/components/Controls.jsx
@@ -0,0 +1,66 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+
+const Controls = () => {
+  const inputElement = useRef();
+
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const handleAdd = () => {
+    dispatch({ type: "ADD", payload: { num: inputElement.current.value } });
+    inputElement.current.value = "";
+  };
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: { num: inputElement.current.value },
+    });
+    inputElement.current.value = "";
+  };
+  return (
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          className="number-input"
+          ref={inputElement}
+          placeholder="Enter Number"
+        />
+        <button type="button" className="btn btn-primary" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Controls;
